9e6f063fbc6a2b12cc65e914395eaa9794449456
SheilaEspinoza <sheilaespinoza43@gmail.com>
2026-01-31 18:08:12 -0500

    Edicion de la vista opcion horarios

--- resources/views/admin/horarios/index.blade.php
+++ resources/views/admin/horarios/index.blade.php
@@ -1,9 +1,25 @@
 @extends('layouts.admin')
 
 @section('content')
-    <h1>Gestión de Horarios</h1>
-    <ul>
-        <li><a href="{{ route('admin.horarios.create') }}">Crear Nuevo Horario</a></li>
-        <li><a href="{{ route('admin.horarios.list') }}">Ver Creaciones</a></li>
-    </ul>
+    <div style="margin-bottom: 20px; text-align: right;">
+        <a href="{{ route('admin.home') }}" class="btn btn-secondary">
+            ← Volver al menú principal
+        </a>
+    </div>
+    <h1 class="title">Gestión de Horarios</h1>
+
+    <div class="dashboard-grid">
+
+        <a href="{{ route('admin.horarios.create') }}" class="dashboard-card">
+            <img src="{{ asset('img/crearHorario.png') }}" alt="Crear Horario">
+            <span>Crear Horario</span>
+        </a>
+
+        <a href="{{ route('admin.horarios.list') }}" class="dashboard-card">
+            <img src="{{ asset('img/verCreaciones.png') }}" alt="Ver Creaciones">
+            <span>Ver Creaciones</span>
+        </a>
+
+    </div>
+
 @endsection
